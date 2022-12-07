db1e4267b05c9ca7c6dd843aebfc6dd7faddbb28
unknown <shubhamteli203@gmail.com>
2022-12-07 07:05:21 +0530

    MySqlConn.

---
+++ SimpleMySqlConnectionApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost; uid=root;password=password;database=classicmodels";
+            MySqlCommand cmd = new MySqlCommand();
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandText = "DROP PROCEDURE IF EXISTS add_emp";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "DROP TABLE IF EXISTS emp";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "CREATE TABLE emp (" +
+                                  "empno INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY," + 
+                                  "first_name VARCHAR(20), last_name VARCHAR(20), birthdate DATE)";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE PROCEDURE add_emp(" +
+                                  "IN fname VARCHAR(20), IN lname VARCHAR(20), IN bday DATETIME, OUT empno INT)" +
+                                  "BEGIN INSERT INTO emp(first_name, last_name, birthdate) " +
+                                  "VALUES(fname, lname, DATE(bday)); SET empno = LAST_INSERT_ID(); END";
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine ("Error " + ex.Number + " has occurred: " + ex.Message);
+                Console.WriteLine(ex.Message);
+            }
+            conn.Close();
